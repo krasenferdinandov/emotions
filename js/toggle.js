9c7732a141afbef5bd6f52297e9e6426d09180e8
krasenferdinandov <krasenferdinandov@gmail.com>
2017-06-04 12:20:09 +0300

    Add files via upload

---
+++ js/toggle.js
@@ -0,0 +1,10 @@
+<script>
+function myFunction(y) {
+    var x = document.getElementById(y);
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+</script>

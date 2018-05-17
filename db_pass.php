b4e1ac3c5c352056f71dcf5569e8491a0ab7f618
Krasen Ferdinandov <krasenferdinandov@gmail.com>
2018-05-17 21:38:47 +0300

    Update db_pass.php

--- db_pass.php
+++ db_pass.php
@@ -1,8 +1,8 @@
 <?php
 	define("URL", "http://testrain.info/");
-	define("DB", "ybmklsfy_krasen");
-	define("USERNAME", "ybmklsfy");
-	define("PASSWORD", "cg7)b3X_eXNV");
+	define("DB", "anydb");
+	define("USERNAME", "anyuser");
+	define("PASSWORD", "anypass");
 	define("HOST", "localhost");
 	$pdo = new PDO('mysql:host=localhost;dbname='.DB.';charset=utf8', USERNAME, PASSWORD);
 	$pdo->exec("set names utf8");
